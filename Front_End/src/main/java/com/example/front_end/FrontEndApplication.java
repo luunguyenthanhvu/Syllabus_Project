46825761779d9c580815ee365ec4338486f7486c
luunguyenthanhvu <21130612@st.hcmuaf.edu.vn>
2024-02-03 18:29:40 +0700

    push code login form

---
+++ Front_End/src/main/java/com/example/front_end/FrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.front_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontEndApplication.class, args);
+	}
+
+}
---
+++ Front_End/src/test/java/com/example/front_end/FrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.front_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
